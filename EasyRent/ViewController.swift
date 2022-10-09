c0aa941faa778106f1804616346c4f5b8f844c62
Hitesh <work@ihitesh.com>
2022-10-09 19:56:40 -0400

    initial commit

---
+++ EasyRent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyRent
+//
+//  Created by Hitesh on 09/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
