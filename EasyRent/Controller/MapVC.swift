0f3cae7ca489b0222221f81fa34e3b6536efad10
Hitesh <work@ihitesh.com>
2022-11-24 18:30:33 -0500

    Map added

--- EasyRent/Controller/MapVC.swift
+++ EasyRent/Controller/MapVC.swift
@@ -35,15 +35,7 @@ class MapVC: UIViewController {
         let camera = GMSCameraPosition.camera(withLatitude: 43.76194, longitude: -79.41028, zoom: 6.0)
         let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
         self.view.addSubview(mapView)
-        
-        /*
-        // Creates a marker in the center of the map.
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.map = mapView
-        */
+    
     }
     
 
